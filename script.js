5e5047d81a0ed3e77ad5627bdb77e06dd7b1f505
ashudangi20 <ashutoshdangi20@gmail.com>
2022-12-28 09:44:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let text = document.getElementById('evaluatedText');
+let letterCount = document.getElementById('letterCount');
+
+text.addEventListener('keyup',(e) => {
+    if(e.key !== "Escape"){
+        
+    }
+    let str = text.value;
+    letterCount.innerHTML = str.length;
+    console.log(str.length);
+})
+
